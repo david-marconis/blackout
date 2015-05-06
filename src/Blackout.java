56f5269457fe1f02cd15c3117e7465a029285aac
David Riddervold Marconis <david@marconis.no>
2015-05-06 19:17:44 +0200

    Finished the contextmenu, should be functinoal now.

--- src/Blackout.java
+++ src/Blackout.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -8,45 +10,50 @@ import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
-
 public class Blackout {
 	private JPopupMenu menu;
 	private JFrame frame;
 	private Point mouseLocation;
 	private Point frameSize = new Point();
 	private int button;
-	private static final int MIN_SIZE = 20;
-	
+	private static final int MIN_SIZE = 30;
+
 	public static void main(String[] args) {
 		new Blackout();
 	}
-	
+
 	public Blackout() {
 		initializeFrame();
 		initializeContextMenu();
 	}
 
 	private void initializeFrame() {
-		frame = new JFrame();
+		frame = new JFrame("Blackout");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setSize(500, 200);
 		frame.setLocationRelativeTo(null);
 		frame.setUndecorated(true);
-		
+		frame.setAlwaysOnTop(true);
+
 		MListener mListener = new MListener();
 		frame.addMouseListener(mListener);
 		frame.addMouseMotionListener(mListener);
 		frameSize = new Point(frame.getLocation());
-		
+
 		frame.setVisible(true);
 	}
 
 	private void initializeContextMenu() {
 		menu = new JPopupMenu("ContextMenu");
-		menu.add(new JMenuItem("Black"));
-		menu.add(new JMenuItem("White"));
-		menu.add(new JMenuItem("Exit"));
+		JMenuItem color = new JMenuItem("White");
+		JMenuItem exit = new JMenuItem("Exit");
+		MenuListener ml = new MenuListener(color, exit);
+		color.addActionListener(ml);
+		exit.addActionListener(ml);
+		menu.add(color);
+		menu.addSeparator();
+		menu.add(exit);
 	}
 
 	public class MListener implements MouseListener, MouseMotionListener {
@@ -57,7 +64,7 @@ public class Blackout {
 				menu.show(me.getComponent(), me.getX(), me.getY());
 			}
 		}
-		
+
 		@Override
 		public void mouseDragged(MouseEvent me) {
 			if (button == MouseEvent.BUTTON1) {
@@ -68,25 +75,61 @@ public class Blackout {
 			} else if (button == MouseEvent.BUTTON3) {
 				int width = (int) (me.getX() + frameSize.getX() - mouseLocation.getX());
 				int height = (int) (me.getY() + frameSize.getY() - mouseLocation.getY());
-				frame.setSize(Math.max(MIN_SIZE, width), Math.max(MIN_SIZE, height));
+				frame.setSize(Math.max(MIN_SIZE, width),
+						Math.max(MIN_SIZE, height));
 				frame.revalidate();
 			}
 		}
+
 		@Override
 		public void mousePressed(MouseEvent me) {
 			button = me.getButton();
 			mouseLocation = me.getPoint();
 			frameSize.setLocation(frame.getWidth(), frame.getHeight());
 		}
+
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
 			button = 0;
 		}
+
 		@Override
-		public void mouseEntered(MouseEvent arg0) {/* Do nothing */}
+		public void mouseEntered(MouseEvent arg0) {/* Do nothing */
+		}
+
 		@Override
-		public void mouseExited(MouseEvent arg0) {/* Do nothing */}
+		public void mouseExited(MouseEvent arg0) {/* Do nothing */
+		}
+
 		@Override
-		public void mouseMoved(MouseEvent arg0) {/* Do nothing */}
+		public void mouseMoved(MouseEvent arg0) {/* Do nothing */
+		}
+	}
+
+	public class MenuListener implements ActionListener {
+		JMenuItem color;
+		JMenuItem exit;
+
+		public MenuListener(JMenuItem color, JMenuItem exit) {
+			this.color = color;
+			this.exit = exit;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent ae) {
+			if (ae.getSource() == color) {
+				if (color.getText().equals("White")) {
+					frame.getContentPane().setBackground(Color.WHITE);
+					color.setText("Black");
+				} else {
+					frame.getContentPane().setBackground(Color.BLACK);
+					color.setText("White");
+				}
+				frame.revalidate();
+			} else if (ae.getSource() == exit) {
+				frame.dispose();
+				System.exit(0);
+			}
+		}
 	}
 }
